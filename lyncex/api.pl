b400633090b93eeec9625fa8958163f0f69a56fe
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2020-02-13 00:01:38 +0100

    GET API: WIP

--- lyncex/api.pl
+++ lyncex/api.pl
@@ -1,10 +1,11 @@
 :- module(api, []).
 
 :- use_module(library(http/http_client)).
+:- use_module(library(http/http_parameters)).
 :- use_module(library(semweb/turtle)).
 
 :- http_handler(root('_api'), ingest, [method(post)]).
-%:- http_handler(root('_api/query'), query, [method(post)]).
+:- http_handler(root('_api/query'), query, [method(get)]).
 
 ingest(Request) :-
     memberchk(content_type('text/turtle'), Request),
@@ -17,13 +18,18 @@ ingest(Request) :-
     format('Content-Type: text/html~n~n'),
     format('OK').
 
-%query(Request) :-
-%    memberchk(content_type('application/x-prolog'), Request),
-%    http_read_data(Request, Input, [
-%        to(atom),
-%        input_encoding('utf-8')
-%    ]),
-%    term_to_atom(Code,Input),
-%    findall(A, Code, Out),
-%    format('Content-Type: text/html~n~n'),
-%    format(Out).
+query(Request) :-
+    http_parameters(Request, [
+        subject(Subject, [optional(true)]),
+        predicate(Predicate, [optional(true)]),
+        object(Object, [optional(true)])
+    ]),
+    findall([Subject, Predicate, Object],rdf(Subject,Predicate, Object), Outputs),
+    format('Content-Type: text/plain~n~n'),
+    forall(member(Output, Outputs),(
+        % TODO: Fails with crud.ttl 
+        atomic_list_concat(Output,' ', OutLine),
+        format(OutLine),
+        format('~n')
+    )).
+
