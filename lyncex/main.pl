cac90c80909e6dcd6149dd7d9745d93df13a901d
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2020-03-28 23:40:13 +0100

    TextController

--- lyncex/main.pl
+++ lyncex/main.pl
@@ -1,15 +1,28 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_unix_daemon)).
+:- use_module(library(semweb/rdfs)).
+:- use_module(library(semweb/rdf11)).
 
 :- use_module('api.pl').
+:- use_module('prefix.pl').
 
-:- http_handler(root(.), index, [method(get)]).
+:- http_handler(root(Path), index(Path, Method), [method(Method)]).
 
 
-index(_Request) :-
-    format('Content-Type: text/html~n~n'),
-    format('OK').
+% TextController
+index(Path, Method, Request) :-
+    rdfs_individual_of(Controller, lyncex:'TextController'),
+    rdf(Controller, lyncex:url, Path^^xsd:string),
+    rdf(Controller, lyncex:method, ControllerMethod^^xsd:string),
+    atom_string(Method, ControllerMethod),
+    rdf(Controller, lyncex:content, Content^^xsd:string),
+    rdf(Controller, lyncex:mime, ContentMIME^^xsd:string),
+    format('Content-Type: '),format(ContentMIME),format('~n~n'),
+    format(Content).
+
+index(Path, Method, Request) :-
+    http_404([], Request).
 
 run :-
     http_daemon([port(11011),fork(false)]).
--- lyncex/prefix.pl
+++ lyncex/prefix.pl
@@ -5,3 +5,4 @@
 :- rdf_register_prefix(rdf, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#').
 :- rdf_register_prefix(rdfs, 'http://www.w3.org/2000/01/rdf-schema#').
 :- rdf_register_prefix(xsd, 'http://www.w3.org/2001/XMLSchema#').
+:- rdf_register_prefix(lyncex, 'https://lyncex.com/lyncex#').
