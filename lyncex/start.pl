522975cf573fdb1c5c38fb00473fbdf4a47baeff
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2020-04-10 20:28:27 +0200

    Custom 404 pages

--- lyncex/start.pl
+++ lyncex/start.pl
@@ -1,4 +1,9 @@
 :- set_test_options([load(never)]).
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_header)).
+:- use_module(library(http/http_unix_daemon)).
+:- use_module(library(http/html_write)).
 
 :- use_module('main.pl').
 
